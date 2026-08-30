12074c709167224160dba7ee59e7d419e5ce8fb8
sadam77478 <85546605+sadam77478@users.noreply.github.com>
2026-08-30 12:40:54 +0500

    Fix loading UI dimension import

--- app/src/main/java/com/sadam/thumbtype/mobile/MainActivity.kt
+++ app/src/main/java/com/sadam/thumbtype/mobile/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sadam.thumbtype.mobile.app.ThumbTypeViewModel
